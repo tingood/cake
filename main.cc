0e715c21c69c582a2e7485bebb338aadde21ddb1
GM Crops <gmcrops1234@mail.com>
2013-11-01 13:48:40 +0000

    Added a third thread

--- main.cc
+++ main.cc
@@ -9,5 +9,6 @@ void f()
 int main ()
 {
   auto t1 = std::async( std::launch::async , f );
-    std::cout << "The cake is a lie" << std::endl;
+  auto t2 = std::async( std::launch::async , f );
+  std::cout << "The cake is a lie" << std::endl;
 }
